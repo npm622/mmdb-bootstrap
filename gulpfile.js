0c83a1cd86052f9d8a081bbf455b6cf509f22697
Nick Makes <nick.makes@mlb.com>
2015-12-21 18:53:26 -0500

    Adds required js files to dist/ for more standalone bower package

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,23 @@ var gulp = require( 'gulp' ), bower = require( 'gulp-bower' ), less = require( '
 var config = {
 	bowerDir : './bower_components',
 	lessInput : './src/less/mmdb-bootstrap.less',
-	lessOutput : './dist/css/'
+	lessOutput : './dist/css/',
+	jsOutput : './dist/js/'
+
 }
 
 gulp.task( 'bower', function() {
     return bower().pipe( gulp.dest( config.bowerDir ) )
 } );
 
+gulp.task ( 'bootstrap', function() {
+	gulp.src( [ '/bootstrap/dist/js/bootstrap.js', '/jquery/dist/jquery.js' ].map( function(e) {
+		return config.bowerDir + e;
+	} ) ).pipe( gulp.dest( config.jsOutput ) );
+} );
+
 gulp.task( 'less', function () {  
   gulp.src( config.lessInput ).pipe( less() ).pipe( gulp.dest( config.lessOutput ) );
 } );
 
-gulp.task( 'default', [ 'bower', 'less' ] );
+gulp.task( 'default', [ 'bower', 'bootstrap', 'less' ] );
